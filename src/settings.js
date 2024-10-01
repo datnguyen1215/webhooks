d5a72cade7ef0cccba4af1a3feed6e2959181ba1
Dat Nguyen <datnguyen1215@gmail.com>
2024-10-01 01:16:49 -0700

    update default port as 9500.

--- src/settings.js
+++ src/settings.js
@@ -6,18 +6,13 @@
 import 'dotenv/config';
 
 const get = () => {
-  if (!process.env.HTTP_PORT)
-    throw new Error('', {
-      cause: { code: 'MISSING_ENV', env: 'HTTP_PORT' }
-    });
-
   return {
     redmine: {
       url: process.env.REDMINE_URL,
       apiKey: process.env.REDMINE_API_KEY
     },
     http: {
-      port: process.env.HTTP_PORT
+      port: process.env.HTTP_PORT || 9500
     },
     gitlab: {
       access_token: process.env.GITLAB_ACCESS_TOKEN,
